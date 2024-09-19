afeb22170b0bd5ec8199e1b911686ab824e4edb9
Berta <bertagonzalezmartin@gmail.com>
2024-09-19 15:51:57 +0200

    Add Button component tests and its configuration

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest"
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+  describe("Given a Button component", () => {
+    describe("When it receives the text 'Hola'", () => {
+      test("Then it renders Button with text 'Hola'", () => {
+        const buttonText = "Hola";
+  
+        render(<Button text="Hola" />);
+        const buttonElement = screen.getByText(buttonText);
+        
+        expect(buttonElement).toBeInTheDocument();
+    })
+  });
+});
