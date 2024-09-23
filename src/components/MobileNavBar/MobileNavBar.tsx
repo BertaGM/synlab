6d092b7c6b17927609e341426b8268f985148433
Berta <bertagonzalezmartin@gmail.com>
2024-09-23 11:49:19 +0200

    Add MobileNavBar component, styles and test

---
+++ src/components/MobileNavBar/MobileNavBar.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../NavBar/NavBar";
+import "./MobileNavBar.css";
+
+const MobileNavBar = () => {
+  return (
+    <div className="mobile-navbar">
+      <NavBar />
+    </div>
+  );
+};
+
+export default MobileNavBar;
