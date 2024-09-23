e8360fe946ff61fdcf655551f8b981c93780647a
Berta <bertagonzalezmartin@gmail.com>
2024-09-23 12:03:29 +0200

    Add DesktopSidebar component, styles and test

---
+++ src/components/DesktopSidebar/DesktopSidebar.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../NavBar/NavBar";
+import "./DesktopSidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Sidebar;
